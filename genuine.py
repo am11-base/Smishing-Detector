66b5045540cf0a924d416a0f4fa4914d834d4fd5
ameen <ameenhneema@gmail.com>
2023-02-16 14:05:42 +0530

    oversampling

---
+++ genuine.py
@@ -0,0 +1,28 @@
+import re
+emailPattern = re.compile(("([a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`" "{|}~-]+)*(@|\sat\s)(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(\.|" "\sdot\s))+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)"))
+def email_check(line):
+    if emailPattern.search(line) is not None:
+        return 1
+    else:
+        return 0
+    
+def http_check(line):
+    http = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+    if not http:
+        return 0
+    else:
+        return 1
+    
+phonePattern = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$')
+
+def phoneNumber_check(line):
+    if phonePattern.search(line) is not None:
+        return 1
+    else:
+        return 0
+
+user_input=input("Enter the SMS: ")
+if(email_check(user_input) or http_check(user_input) or phoneNumber_check(user_input)) :
+    print("Further check needed")
+else :
+    print("Genuine") 
